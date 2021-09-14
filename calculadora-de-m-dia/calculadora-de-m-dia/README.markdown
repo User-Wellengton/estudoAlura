a0afc27bde60f6f470b331d8b14229d445a0324f
Wellengton José Machado <89493922+User-Wellengton@users.noreply.github.com>
2021-09-14 09:54:55 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/User-Wellengton/pen/rNwzjPO](https://codepen.io/User-Wellengton/pen/rNwzjPO).
+
+
---
+++ calculadora-de-m-dia/calculadora-de-m-dia/src/script.js
@@ -0,0 +1,13 @@
+var nome = " Wellengton "
+var n1 = 9
+var n2 = 6
+var n3 = 7
+var n4 = 8
+var MEDIA = (n1 + n2+ n3+ n4) / 4
+var notaFinal = MEDIA.toFixed(1)
+console.log(" Bem vindo!"+ nome)
+console.log(notaFinal)
+
+
+// revisão
+//variaveis,strings, console.log, toFixed, operaçãos matematicas, contatenação, 
